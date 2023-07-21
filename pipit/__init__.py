609f5bdc1c3fd5979c973005ec1a8fbe6ef87739
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2023-07-21 10:12:56 -0600

    Update __init__.py

--- pipit/__init__.py
+++ pipit/__init__.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),".."))
+
 
 __version__ = '0.1.0'
