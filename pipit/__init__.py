47decacf73d6f9de49859c6bef4060605509b91d
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2023-07-20 23:24:08 -0600

    __init__ improved

--- pipit/__init__.py
+++ pipit/__init__.py
@@ -1,1 +1,4 @@
+import configs,core,stats,toolkit
+
+
 __version__ = '0.1.0'
--- pipit/toolkit.py
+++ pipit/toolkit.py
@@ -31,38 +31,44 @@ def bwa_index(refrence: pathlib.Path,
         
 
 def bwa_align(refrence: pathlib.Path,
-              fastq: pathlib.Path,
-              output_file: pathlib.Path,
-              container:str="none",
-              number_of_threads:int=1,
-              config:configs.Containers=configs.Containers())->str:
-    """ This function will return the command to align the fastq file to the refrence genome using bwa.
+                fastq_1: pathlib.Path,
+                fastq_2: pathlib.Path,
+                number_of_threads: int,
+                output_file: pathlib.Path,
+                container:str="none",
+                config:configs.Containers=configs.Containers())->str:
+                
+    """ This function will return the command to align the fastq files to the refrence genome using bwa.
     Args:
         refrence (pathlib.Path): The path to the refrence sequence file.
-        fastq (pathlib.Path): The path to the fastq file.
+        fastq_1 (pathlib.Path): The path to the first fastq file.
+        fastq_2 (pathlib.Path): The path to the second fastq file.
+        number_of_threads (int): The number of threads to use.
         output_file (pathlib.Path): The path to the output file.
         container (str): The name of the container to run the command in:
         choose from "none","singularity","docker".
-        number_of_threads (int): The number of threads to use.
     Returns:
-        str: The command to align the fastq file to the refrence genome.
-
+        str: The command to align the fastq files to the refrence genome.
     """
     if not refrence.exists():
         raise FileNotFoundError(f"{refrence} not found")
+
+    if not fastq_1.exists():
+        raise FileNotFoundError(f"{fastq_1} not found")
     
-    if not fastq.exists():
-        raise FileNotFoundError(f"{fastq} not found")
-    
+    if not fastq_2.exists():
+        raise FileNotFoundError(f"{fastq_2} not found")
+
     if container == "none":
-        cmd = f"bwa mem -t {number_of_threads} {str(refrence)} {str(fastq)} > {str(output_file)}"
-    
+        cmd = f"bwa mem -t {number_of_threads} {str(refrence)} {str(fastq_1)} {str(fastq_2)} > {str(output_file)}"
+
     elif container == "singularity":
-        cmd = f"singularity exec --bind {str(refrence.parent)}:{str(refrence.parent)} {config.singularity['bwa']} bwa mem -t {number_of_threads} {str(refrence)} {str(fastq)} > {str(output_file)}"
+        cmd = f"singularity exec --bind {str(refrence.parent)}:{str(refrence.parent)} {config.singularity['bwa']} bwa mem -t {number_of_threads} {str(refrence)} {str(fastq_1)} {str(fastq_2)} > {str(output_file)}"
 
     elif container == "docker":
-        cmd = f"docker run -v {str(refrence.parent)}:{str(refrence.parent)} {config.docker['bwa']} bwa mem -t {number_of_threads} {str(refrence)} {str(fastq)} > {str(output_file)}"
+        cmd = f"docker run -v {str(refrence.parent)}:{str(refrence.parent)} {config.docker['bwa']} bwa mem -t {number_of_threads} {str(refrence)} {str(fastq_1)} {str(fastq_2)} > {str(output_file)}"
     
     return cmd
+                
 
 
