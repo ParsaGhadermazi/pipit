b0ed22daaa7ddc552ebf31d12e4edec920ac83ce
ParsaGhadermazi <54489047+ParsaGhadermazi@users.noreply.github.com>
2025-06-13 10:42:26 -0600

    Update __init__.py

--- bioplumber/__init__.py
+++ bioplumber/__init__.py
@@ -1,13 +1,22 @@
 import sys
 import os
 import json
+import pathlib
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 __version__ = '0.1.0'
 
-with open(os.path.join(os.path.dirname(__file__), 'configs.json'), 'r') as f:
-    config = json.load(f)
+def get_config():
+    """Get the current configuration."""
+    with open(os.path.join(os.path.dirname(__file__), 'configs.json'), 'r') as f:
+        config = json.load(f)
+    return config
+
+config=get_config()
+if not pathlib.Path(config.get('base_directory')):
+    config['base_directory'] = ""
+
     
 def set_base_directory(path):
     """Set the base directory for the application."""
@@ -17,8 +26,3 @@ def set_base_directory(path):
     with open(os.path.join(os.path.dirname(__file__), 'configs.json'), 'w') as f:
         json.dump(config, f, indent=4)
 
-def get_config():
-    """Get the current configuration."""
-    with open(os.path.join(os.path.dirname(__file__), 'configs.json'), 'r') as f:
-        config = json.load(f)
-    return config
